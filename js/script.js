e7cb30e849e611c7975d2027c89fcf0f15e3a313
milton <milton.website@gmail.com>
2018-05-12 18:27:04 -0500

    Se creo el repositorio pagina web milton.website

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let btnMenu = document.getElementById('btn-menu');
+let contentMenu = document.getElementById('content-menu');
+  btnMenu.addEventListener('click', function(){
+    contentMenu.classList.toggle('show-menu')
+  })
